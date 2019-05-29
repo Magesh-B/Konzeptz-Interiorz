5b971d5b9e8fab996f1d157e50169cabde98c7d2
Magesh B <magesh.madz@gmail.com>
2019-05-29 22:07:26 +0530

    updated

--- script.js
+++ script.js
@@ -16,5 +16,20 @@ window.addEventListener('load', function()
 */
 
 $(window).on('load',function(){
-  $('.loader').fadeOut();
+  $('#loader').fadeOut('slow');
 });
+
+
+/*
+document.onreadystatechange = function () {
+  var state = document.readyState
+  if (state == 'interactive') {
+       document.getElementById('contents').style.visibility="hidden";
+  } else if (state == 'complete') {
+      setTimeout(function(){
+         document.getElementById('interactive');
+         document.getElementById('loader').style.visibility="hidden";
+         document.getElementById('contents').style.visibility="visible";
+      },1000);
+  }
+}*/
